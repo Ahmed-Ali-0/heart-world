323289d499eacdfdac14dc41b965c4ddf134305c
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-09-22 14:38:56 -0400

    Flashing emotions 😁

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Sad)
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Silly)
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
 })
